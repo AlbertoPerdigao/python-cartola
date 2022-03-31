953076cb350d6bf737c70c9265f626f70163680f
Alberto Perdigão <alberto.perdigao@gmail.com>
2022-03-31 11:39:24 -0300

    Deleted the database.py.

--- app/database.py
+++
@@ -1,5 +0,0 @@
-HOST = "ec2-3-229-161-70.compute-1.amazonaws.com"
-DATABASE = "dcs18oo4dqigda"
-PORT = 5432
-USER = "xgzvdkjopsxuxg"
-PASSWORD = "f8bb0032516385c37c2cfa153a5629d55e9c6058223c12bce5cada0e15f09c81"
